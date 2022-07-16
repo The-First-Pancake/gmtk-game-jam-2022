fc67700fc0da21390195ec0d3c1d951641388226
George Philbrick <gphilbri@gmail.com>
2022-07-16 13:31:37 -0400

    Count dice pool

---
+++ Assets/Scripts/DicePoolManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PoolResults {
+    public int [] pips = new int[Enum.GetNames(typeof(Pip.PipType)).Length];
+}
+
+public class DicePoolManager : MonoBehaviour
+{
+    public List<Dice> diceInPool;
+    public PoolResults poolResults;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponentsInChildren<Dice>(diceInPool);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        poolResults = GetPoolResults();
+    }
+
+    void AddDieToPool(GameObject die) {
+        // add die to children
+        die.transform.parent = transform;
+        diceInPool.Add(die.GetComponent<Dice>());
+    }
+
+    void RemoveDieFromPool(GameObject die) {
+        die.transform.parent = null;
+        diceInPool.Remove(die.GetComponent<Dice>());
+    }
+
+    PoolResults GetPoolResults() {
+        PoolResults poolResults = new PoolResults();
+        foreach (Dice die in diceInPool) {
+            Face upFace = die.getUpFace();
+            if (upFace != null) {
+                foreach (Pip pip in upFace.pips) {
+                    poolResults.pips[((int)pip.piptype)] += 1;
+                }
+            }
+        }
+        return poolResults;
+    }
+}
