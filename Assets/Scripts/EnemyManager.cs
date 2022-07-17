391a4210f1318d874abbe83e27e90117d5d7592e
Declan Morrison <declan@chromajam.com>
2022-07-17 10:42:16 -0400

    Adds alien slide in

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -10,14 +10,18 @@ public class EnemyManager : MonoBehaviour
     private GameObject DeathParticles;
     public GameObject EnemyPrefab;
     public GameObject GameScene;
+    public GameObject EnemySceneMarker;
     private ParticleSystem DeathParticlesSystem;
     public UnityEvent OnNewEnemy;
+
+    private bool isEnemySliding;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        isEnemySliding = false;
         Enemy = GameObject.FindGameObjectWithTag("Enemy");
         DeathParticles = GameObject.FindGameObjectWithTag("Enemy Death Particle");
         DeathParticlesSystem = DeathParticles.GetComponent<ParticleSystem>();
@@ -38,12 +42,15 @@ public class EnemyManager : MonoBehaviour
     }
 
     private void EnemySlideIntoView() {
-        Enemy.transform.TransformDirection(Vector3.left);
+        if (Enemy != null && (Enemy.transform.position.x > EnemySceneMarker.transform.position.x)) {
+            float speedOffset = (Enemy.transform.position.x / EnemySceneMarker.transform.position.x) * 0.1f;
+            Enemy.transform.Translate(Vector3.forward * speedOffset);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemySlideIntoView();
     }
 }
