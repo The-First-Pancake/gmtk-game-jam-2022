3e54e70d12d2f2e92a9435503ee08e0b251203b6
George Philbrick <gphilbri@gmail.com>
2022-07-17 12:11:00 -0400

    Dice Sounds

--- Assets/Scripts/DiceRoller.cs
+++ Assets/Scripts/DiceRoller.cs
@@ -22,12 +22,15 @@ public class DiceRoller : MonoBehaviour
     private float debouncedVelocity = 0;
     private int debounceCounter = 0;
     private bool emitSettled = false;
+
+    private AudioSource diceSound;
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         camera = GameManager.instance.camera;
+        diceSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -59,6 +62,10 @@ public class DiceRoller : MonoBehaviour
                 }
                 break;
         }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        diceSound.Play();
     }
 
     bool CheckDrop() {
