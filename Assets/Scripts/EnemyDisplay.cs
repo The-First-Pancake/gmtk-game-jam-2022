24c0221404ec9f20e68422790d31d3bef737c64f
Oliver Philbrick <oliveman521@gmail.com>
2022-07-17 09:08:36 -0700

    UI tweaks

--- Assets/Scripts/EnemyDisplay.cs
+++ Assets/Scripts/EnemyDisplay.cs
@@ -13,7 +13,7 @@ public class EnemyDisplay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        canvasObject.GetComponent<Canvas>().worldCamera = GameManager.instance.camera;
     }
 
     // Update is called once per frame
