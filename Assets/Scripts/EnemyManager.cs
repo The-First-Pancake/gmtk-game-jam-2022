e047614d93c1adc9eb95d07f2104ff1fd06b632e
Declan Morrison <declan@chromajam.com>
2022-07-17 13:33:44 -0400

    Adds enemy leave

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -10,9 +10,12 @@ public class EnemyManager : MonoBehaviour
     private GameObject DeathParticles;
     public GameObject EnemyPrefab;
     public GameObject GameScene;
-    public GameObject EnemySceneMarker;
+    public GameObject EnemySceneMarkerIn;
+    public GameObject EnemySceneMarkerOut;
     private ParticleSystem DeathParticlesSystem;
     public UnityEvent OnNewEnemy;
+    public Transform cam;
+    private bool PlayerLost = false;
 
 
 
@@ -37,15 +40,45 @@ public class EnemyManager : MonoBehaviour
     }
 
     private void EnemySlideIntoView() {
-        if (Enemy != null && (Enemy.transform.position.x > EnemySceneMarker.transform.position.x)) {
-            float speedOffset = (Enemy.transform.position.x / EnemySceneMarker.transform.position.x) * 0.1f;
+        if (Enemy != null && (Enemy.transform.position.x > EnemySceneMarkerIn.transform.position.x)) {
+            float speedOffset = (Enemy.transform.position.x / EnemySceneMarkerIn.transform.position.x) * 0.1f;
             Enemy.transform.Translate(Vector3.forward * speedOffset);
         }
+    }
+
+    private void EnemyRotateAway() {
+        if (cam.InverseTransformPoint(Enemy.transform.position).z > -100 && cam.InverseTransformPoint(Enemy.transform.position).z <= 100) {
+            Enemy.transform.Translate(new Vector3(0, 0, -1), Space.Self);
+        }
+
+        if (Enemy.transform.rotation.y < 90) {
+            Enemy.transform.Rotate(Vector3.up, 1f);
+        }
+    }
+
+    private void EnemySlideOutView() {
+        if (Enemy != null && (Enemy.transform.position.x < EnemySceneMarkerOut.transform.position.x)) {
+            float speedOffset = (EnemySceneMarkerOut.transform.position.x / Enemy.transform.position.x) * 0.1f;
+            Enemy.transform.Translate(Vector3.forward * speedOffset);
+        } else if (Enemy != null && (Enemy.transform.position.x >= EnemySceneMarkerOut.transform.position.x)) {
+            Destroy(Enemy);
+            PlayerLost = false;
+        }
+    }
+
+    public void PlayerLose() {
+        Debug.Log("PlayerLose");
+        PlayerLost = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        EnemySlideIntoView();
+        if (PlayerLost){
+            // EnemyRotateAway();
+            EnemySlideOutView();
+        } else {
+            EnemySlideIntoView();
+        }
     }
 }
