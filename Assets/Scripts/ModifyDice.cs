8a83cabd4897a8913f3086f136f50366a48a941d
Oliver Philbrick <oliveman521@gmail.com>
2022-07-16 18:32:10 -0700

    Pip Updates

--- Assets/Scripts/ModifyDice.cs
+++ Assets/Scripts/ModifyDice.cs
@@ -4,45 +4,73 @@ using UnityEngine;
 
 public class ModifyDice : MonoBehaviour
 {
-    private Transform diceZoomPoint;
+    private Transform zoomPoint;
 
-    float diceSpacing = 2.5f;
+    float objectSpacing = 2.5f;
     float dicerotateSpeed = 60;
     public DicePoolManager dicePoolManager;
     Transform clickedSocket = null;
 
-    public
+    
     void Start()
     {
-        diceZoomPoint = Camera.main.transform.Find("DiceZoomPoint");
-        StartCoroutine(SelectDiceSocket());
+        zoomPoint = Camera.main.transform.Find("DiceZoomPoint");
+        StartCoroutine(SelectDiceSocketFromPool());
     }
 
-    public IEnumerator SelectDiceSocket(System.Action<Transform> selectedSocket = null)
+    public void ChooseAFace()
+    {
+
+        int choices = 3;
+        List<Face> faceOptions = new List<Face>();
+        for (int i = 0; i < choices; i++)
+        {
+            Face face1 = Instantiate(GameManager.instance.getFacePrefab_OfSize(6)).GetComponent<Face>();
+            face1.configureFace(FaceType.pip);
+            faceOptions.Add(face1);
+
+
+        }
+
+    }
+
+
+    public void moveTransformsTo_ZoomPoint(List<Transform> objects)
+    {
+        int count = objects.Count;
+        float span = (count - 1) * objectSpacing;
+        float startX = (-span / 2);
+        for (int i = 0; i < count; i++)
+        {
+            float newX = startX + objectSpacing * i;
+            objects[i].position = zoomPoint.position + new Vector3(newX, 0, 0);
+        }
+    }
+
+    public IEnumerator SelectDiceSocketFromPool(System.Action<Transform> selectedSocket = null)
     {
         yield return null; //wait a frame in case this is happening right away (TODO remove this)
-
         clickedSocket = null;
+
+        
         //freeze dice
         foreach (Dice dice in dicePoolManager.diceInPool)
         {
+            
             dice.diceBody.GetComponent<Rigidbody>().isKinematic = true;
             dice.transform.rotation = Quaternion.Euler(45, 0, 45);
         }
 
-        //move all dice into display
-        int diceCount = dicePoolManager.diceInPool.Count;
-        float span = (diceCount - 1) * diceSpacing;
-        float startX = (-span / 2);
-        for (int i = 0; i < diceCount; i++)
-        {
-            float newX = startX + diceSpacing * i;
-            dicePoolManager.diceInPool[i].transform.position = diceZoomPoint.position + new Vector3(newX, 0, 0);
-        }
+        //make list of transforms so we can align them on the zoom point
+        List<Transform> diceTransformList = new List<Transform>();
+        foreach (Dice dice in dicePoolManager.diceInPool) { diceTransformList.Add(dice.transform); }
+
+        //Move all thee transforms where they need to go
+        moveTransformsTo_ZoomPoint(diceTransformList);
 
 
 
-        while(clickedSocket == null)
+        while (clickedSocket == null)
         {
             foreach (Dice dice in dicePoolManager.diceInPool)
             {
@@ -73,7 +101,8 @@ public class ModifyDice : MonoBehaviour
         foreach (Dice dice in dicePoolManager.diceInPool)
         {
             dice.diceBody.GetComponent<Rigidbody>().isKinematic = false;
-            tra
+            dice.transform.position = GameManager.instance.diceHomePoint.position;
+            yield return new WaitForSeconds(.1f);
         }
 
         Debug.Log(clickedSocket);
--- Assets/Scripts/Pip.cs
+++ Assets/Scripts/Pip.cs
@@ -8,7 +8,8 @@ public class Pip : ScriptableObject
     public Sprite sprite;
     public Color color;
     public PipType type;
-
+    public float rarity = 10;
+    public Vector2 abundance = new Vector2(1, 4);
 }
 public enum PipType
 {
